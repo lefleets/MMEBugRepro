e11ea2f498bfc2e40ec8e415efcb618414902af5
elight <eric@homathkotech.com>
2019-07-12 10:35:16 -0700

    Initial Commit

---
+++ MMEBugRepro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MMEBugRepro
+//
+//  Created by Eric Lightfoot on 2019-07-12.
+//  Copyright © 2019 Eric Lightfoot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
